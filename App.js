7223d6a5fe846200302841ad3d3951b5a38eb64c
Matt Chan <matthew.chan@slalom.com>
2020-08-26 20:01:18 -0400

    [MC] Init React Native Project

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+
+import { getLocationPermission } from './src/utils/location.utils';
+
+import MapViewScreen from './src/screens/MapView.screens';
 
 export default function App() {
+
+  useEffect(() => {
+    getLocationPermission();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MapViewScreen />
+    </SafeAreaView>
   );
 }
 
@@ -17,5 +24,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
